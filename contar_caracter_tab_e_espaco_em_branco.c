a77f5f0d75a21a08a281b46ff14385c5776973a3
Thiago Frois <thiagofrois@outlook.com.br>
2022-02-09 20:08:44 -0300

    Colocando a contagem junto da leitura de caracteres.

--- contar_caracter_tab_e_espaco_em_branco.c
+++ contar_caracter_tab_e_espaco_em_branco.c
@@ -7,8 +7,7 @@ int main(){
     int texto[TAM_TEXTO], i, nc, nl = 0, ntab = 0, nb = 0;
 
     for(nc = 0; nc < TAM_TEXTO && (texto[nc] = getchar()) != EOF; nc++);
-
-    for(i = 0; texto[i] != EOF; i++){
+       for(i = 0; texto[i] != EOF; i++){
         if(texto[i] == '\n')
             nl++;
         if(texto[i] == '\t')
@@ -16,6 +15,7 @@ int main(){
         if(texto[i] == ' ')
             nb++;
     }
+
     nb = nb + 2*ntab;
 
     printf("NC = %d, NL = %d, NTAB = %d, NB = %d\n", nc, nl, ntab, nb);
