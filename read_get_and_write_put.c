2611eab5f0e963e57d29466fb21aba892c24e12a
Thiago Frois <thiagofrois@outlook.com.br>
2022-02-09 19:21:53 -0300

    Arrumando a leitura e escrita.

--- read_get_and_write_put.c
+++ read_get_and_write_put.c
@@ -1,20 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 #define TAM_TEXTO 100
 
-int main(){
+int main()
+{
+    int texto[TAM_TEXTO], i = 0;
 
-    char texto[TAM_TEXTO];
-    int i;
+    for(i = 0; i < TAM_TEXTO && (texto[i] = getchar()) != EOF; i++);
 
-
-    for(i = 0; i < TAM_TEXTO - 1 && ((texto[i] = getchar()) != '\n'); i++);
-
-    texto[i] = '\0';
-
-    for(i = 0; texto[i] != '\0'; i++)
+    for(i = 0; texto[i] != EOF; i++)
         putchar(texto[i]);
-    printf("\n");
 
     return 0;
 }
