a5453993fce9c2419e5b8ccef73fc0613ada8d48
Thiago Frois <thiagofrois@outlook.com.br>
2022-02-10 15:05:21 -0300

    Adicionando condições de existência, ou não existência, do valor procurado no vetor.

--- busca_linear.c
+++ busca_linear.c
@@ -7,17 +7,20 @@ int busca_linear(int arranjo[], int num);
 
 int main(){
 
-    int indice, arranjo[TAM_ARRANJO] = {12,5,82,3,54,2,9,3,23,31}, num = 54;
+    int indice, arranjo[TAM_ARRANJO] = {12,5,82,3,54,2,9,3,23,31}, num = 31;
 
     indice = busca_linear(arranjo, num);
 
-    printf("Está no índice %d do vetor.\n", indice);
+    if(indice != -1)
+        printf("O número %d é o elemento %d do vetor.\n", num, indice + 1);
+    else
+        printf("O número %d não está no vetor.\n", num);
 
     return 0;
 }
 
 int busca_linear(int arranjo[], int num){
-    int indice = 0, i;
+    int indice = -1, i;
     for(i = 0; i < TAM_ARRANJO; i++){
         if(arranjo[i] == num){
             indice = i;
